c9fd980b71f35642d28c6180c720da5704079b10
Xenobikz <reichelleanne.robles@yahoo.com>
2017-08-04 12:51:14 -0400

    Update dictionary-attack.js

--- dictionary-attack.js
+++ dictionary-attack.js
@@ -15,11 +15,11 @@ window.onload = init;
 /* ADD YOUR CODE BELOW */
 
 function checkPassword() {
-  var duck = document.getElementById("pw").value;
+  var password = document.getElementById("pw").value;
   for(var i= 0;i<wordsList.length; i++) {
     if(wordsList[i]=password) {
     document.getElementById("demo").innerHTML;
     break;
   }
-}
+ }
 }
